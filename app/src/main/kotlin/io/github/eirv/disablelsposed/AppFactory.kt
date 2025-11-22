4107b9ce53d1149efc38fc672d302aff95f39e50
Eirv <2114486405@qq.com>
2025-11-22 08:43:25 +0800

    Move Native initialization to companion object

--- app/src/main/kotlin/io/github/eirv/disablelsposed/AppFactory.kt
+++ app/src/main/kotlin/io/github/eirv/disablelsposed/AppFactory.kt
@@ -3,7 +3,9 @@ package io.github.eirv.disablelsposed
 import android.app.AppComponentFactory
 
 class AppFactory : AppComponentFactory() {
-  init {
-    Native.getFlags()
+  companion object {
+    init {
+      Native.getFlags()
+    }
   }
 }
